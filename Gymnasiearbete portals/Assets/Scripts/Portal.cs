e3d3a803fab2ad3f1096d974178388bca9e0b058
Kudowastaken <128066867+Kudowastaken@users.noreply.github.com>
2023-09-01 11:42:46 +0200

    fixed texture and camera issues
    things are working visually excpet for the render textures only rendering black

--- Gymnasiearbete portals/Assets/Scripts/Portal.cs
+++ Gymnasiearbete portals/Assets/Scripts/Portal.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Portal : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class Portal : MonoBehaviour
 
     private void Awake()
     {
+        RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
         playerCam = Camera.main;
         portalCam = GetComponentInChildren<Camera>();
         portalCam.enabled = false;
@@ -19,24 +21,26 @@ public class Portal : MonoBehaviour
 
     private void CreateViewTexture()
     {
+        Debug.LogError("CreateViewTexture was called");
         if (viewTexture == null || viewTexture.width != Screen.width || viewTexture.height != Screen.height)
         {
+            Debug.LogError("Passed the first IF statement");
             if (viewTexture != null)
             {
+                Debug.LogError("Releasing view texture");
                 viewTexture.Release();
             }
-
+            Debug.LogError("Should draw a new render texture");
             viewTexture = new RenderTexture(Screen.width, Screen.height, 0);
             //Render camera view onto the view texture
             portalCam.targetTexture = viewTexture;
             //Set the linked portals texture to the view texture from the main portal
-            linkedPortal.portalScreen.material.SetTexture("_MainTexture", viewTexture);
+            linkedPortal.portalScreen.material.SetTexture("_BaseMap", viewTexture);
         }
     }
 
-    private void OnPreRender()
+    private void OnBeginCameraRendering(ScriptableRenderContext context, Camera camera)
     {
-        Debug.Log("OnPreRender was called");
         portalScreen.enabled = false;
         CreateViewTexture();
         
@@ -44,9 +48,11 @@ public class Portal : MonoBehaviour
         var m = transform.localToWorldMatrix * linkedPortal.transform.worldToLocalMatrix *
                 playerCam.transform.localToWorldMatrix;
         portalCam.transform.SetPositionAndRotation(m.GetColumn(3), m.rotation);
-        
-        portalCam.Render();
 
         portalScreen.enabled = true;
+    }
+    void OnDestroy()
+    {
+        RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
     }
 }
